92340c4512e2a6260d70c869ae2f226ff788d0c5
Sydney Krantz <syd107@gmail.com>
2020-05-26 18:31:27 -0700

    some minor changes to portfolio

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top align-items-right">
       <img src="./assets/logo_2.png"
         />
       <NavLink to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,57 +2,73 @@
 import React from "react";
 import Thumbnail from '../components/Thumbnail'
 import '../App.css'
- 
+
 
 // import hero, nav, footer etc. 
 function Portfolio() {
-    return (
-      
-        <div className="portfolioImage">
-          <Thumbnail
-          link=""
-          image="./assets/planner.png"
-          title="Daily Planner"
-          />
-          <Thumbnail
-          link=""
-          image="./assets/brewlove.png"
-          title="Brewlove"
-          />
-           <Thumbnail
-          link=""
-          image="./assets/bookclub.png"
-          title="Book Club"
-          />
-          <Thumbnail
-          link=""
-          image="./assets/pwdgen.png"
-          title="Password Generator"
-          />
-          <Thumbnail
+  return (
+          <div className="portfolioImage">
+    <div className="container">
+      <div className="row">
+        <div className="col-md">
+            <Thumbnail
+              link=""
+              image="./assets/planner.png"
+              title="Daily Planner"
+            />
+            </div>
+          <div className="col-md">
+            <Thumbnail
+              link=""
+              image="./assets/brewlove.png"
+              title="Brewlove"
+            />
+            </div>
+          <div className="col-md">
+            <Thumbnail
+              link=""
+              image="./assets/bookclub.png"
+              title="Book Club"
+            />
+            </div>
+              <Thumbnail
+              link=""
+              image="./assets/pwdgen.png"
+              title="Password Generator"
+        />
+      </div>
+    </div>
+      <div className="row">
+      <div className="col-md">
+        <Thumbnail
           link=""
           image="./assets/weather.png"
           title="Weather Dashboard"
-          />
-          <Thumbnail
+        /></div>
+        <div className="col-md">
+        <Thumbnail
           link=""
           image="./assets/codingquiz.png"
           title="Coding Quiz"
-          />
-          <Thumbnail
+        /></div>
+        <div className="col-md">
+        <Thumbnail
           link=""
           image="./assets/notetaker.png"
           title="Note Taker"
-          />
-          <Thumbnail
+        /></div>
+        <div className="col-md">
+        <Thumbnail
           link=""
           image="./assets/empdir.png"
           title="Employee Directory"
-          />
-         
-        </div>
-      
-    );
-  }
-  
-  export default Portfolio;
+        /></div>
+      </div>
+
+
+    </div>
+
+  );
+}
+
+export default Portfolio;
