776ea97a945d35d9b9e05d5fd8c63625ffd157ab
Sydney Krantz <syd107@gmail.com>
2020-05-28 13:06:42 -0700

    got rid of flip cards

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -1,34 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import "./style.css";
 
 function Thumbnail(props) {
-  return (
-    <div className="flip-card">
-      <div className="flip-card-inner">
-          <div className="card border-danger mb-3 align-items-center flip-card-front">
-            <div className="card-body hvr-ripple-in hvr-wobble-skew">
-              <div className="project-title">{props.title}</div>
-              <Link to={props.link}>
+    return (
+            <div className="card border-danger mb-3 align-items-center">
+      <div className="card-body hvr-ripple-in hvr-wobble-skew">
+                <div className="project-title">{props.title}</div>
+            <Link to={props.link}>
                 <div className="project-image">
-                  <img src={props.image} alt="Project" />
+                    <img src={props.image} alt="Project"/>
                 </div>
-              </Link>
-            </div>
-          </div>
-          <div className="card border-danger mb-3 align-items-center flip-card-back">
-            <div className="card-body hvr-ripple-in hvr-wobble-skew">
-              <div className="project-title">{props.title}</div>
-              <Link to={props.link}>
-                <div className="project-image">
-                  <img src={props.image} alt="Project" />
-                </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-    </div>
+            </Link>
+        </div></div>
+    )
     
-  );
 }
 export default Thumbnail;
+
